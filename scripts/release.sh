94f8ffcf9b2b22ca50b885f4fee9212104e8f9a4
romainprignon <pro.rprignon@gmail.com>
2017-12-31 00:01:16 +0100

    chore: test release

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ source ./utils.sh
 version_number=$1
 current_version=$(get_current_version)
 last_commit=$(get_last_commit)
-
+echo $last_commit
 if [ -z $version_number ]; then
   version_number=$current_version-$last_commit;
 fi
