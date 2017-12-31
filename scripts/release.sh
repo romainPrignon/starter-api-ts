6bd8c9c138e8238843169f477738a802fc84a887
romainprignon <pro.rprignon@gmail.com>
2017-12-31 14:20:25 +0100

    fix: manual release

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,7 @@ source ./scripts/utils.sh
 npm version --no-git-tag-version $1
 tag=$(get_current_version)
 git add package.json package-lock.json
-git commit -m "Release $tag"
+git commit -m "Release $tag" --no-verify
 
 ./scripts/prepare-release.sh $tag
 ./scripts/push-release.sh
